da259e560ab11334d78be3db35dc538edf33646a
Huy Tran Tuan <trantuanhuyltv@gmail.com>
2025-10-22 21:23:50 +0700

    add tailwindcss and shadcn ui

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -1,22 +1,20 @@
 import { Outlet, Navigate, useLocation } from "react-router";
 
-// import { useCurrentUser } from "@/features/auth";
+import { useCurrentUser } from "@/features/auth";
 import { useSelector } from "react-redux";
 
 function PrivateRoute() {
   const { pathname } = useLocation();
-  // const currentUser = useCurrentUser();
+  const currentUser = useCurrentUser();
   const fetching = useSelector((state) => state.auth.fetching);
 
   if (fetching) {
     return <div>Loading...</div>;
   }
 
-  // if (!currentUser) {
-  //     return (
-  //         <Navigate to={`/login?continue=${encodeURIComponent(pathname)}`} />
-  //     );
-  // }
+  if (!currentUser) {
+    return <Navigate to={`/login?continue=${encodeURIComponent(pathname)}`} />;
+  }
 
   return <Outlet />;
 }
